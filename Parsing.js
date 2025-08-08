6af52fd69e26a56c507ca3fa48f67d9f790a34e2
DevinSuttles <devinfnsuttles@gmail.com>
2025-08-08 08:47:34 -0500

    Problem complete

---
+++ Parsing.js
@@ -0,0 +1,57 @@
+/*Devin Suttles 
+In the programming language of your choice, write a program that parses a sentence and replaces each word with the following: first letter, 
+number of distinct characters between first and last character, and last letter. For example, Smooth would become S3h. 
+Words are separated by spaces or non-alphabetic characters and these separators should be maintained in their original form and location in the answer. 
+A few of the things we will be looking at is accuracy, efficiency, solution completeness. 
+Please include this problem description in the comment at the top of your solution. 
+The problem is designed to take approximately 1-2 hours and will be used as a conversation point in the verbal assessment part of the on-site interview. 
+Please complete the coding assessment and have your solution uploaded as soon as possible. 
+Please upload your solution to Dropbox or similar option (not G-Drive) and send a link to download the folder prior to you interview*/
+
+const wordTransformer = (word) => {
+    // words must be at least 3 charcaters long to be transformed
+    if (word.length < 3) {
+        return word;
+    }
+
+
+    let dictionary = {
+    };
+    
+    let countUniqueCharacters = 0;
+    for (let i = 1; i < word.length-1; i++) {
+        if (!dictionary[word[i]]) {
+            dictionary[word[i]] = true;
+            countUniqueCharacters++;
+        }
+    }
+
+        return word[0] + countUniqueCharacters + word[word.length - 1];
+};
+
+const parseWords = (sentence) => {
+    if (sentence.length === 0) {
+        return '';
+    }
+    
+    let regex = /([a-zA-Z]+|[^a-zA-Z])/g;
+    let array = sentence.match(regex);
+    
+    let transformedParts = array.map(word => {
+        if (/^[a-zA-Z]$/.test(word)) {
+            return wordTransformer(word);
+        }
+        return word;
+    });
+
+    return transformedParts.join('');
+};
+
+
+// Test cases
+console.log(parseWords("Smooth"));
+console.log(parseWords("A smooth criminal!"));
+console.log(parseWords("Go chiefs Go!!!!"));
+console.log(parseWords("A smooth criminal"));
+console.log(parseWords("Den of theives is a good movie"));
+console.log(parseWords(""));
